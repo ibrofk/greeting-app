edf32e64a2cebe9cf8f54c62e62828c7d258ead1
ibrofk <114627759+ibrofk@users.noreply.github.com>
2022-09-29 02:47:47 +0200

    added new names in names string

--- names.py
+++ names.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import random
-names = 'ibrahim omer ahmed frok micke 3alli both jacob isma3il'
+names = 'ibrahim omer ahmed frok micke 3alli both jacob isma3il Alba, Jessica Alberti Leone Battista Albom Mitch Alcott Louisa May Alcuin Aldiss Brian Alexander Alexie Sherman'
 def greet(name):
    print('my name is AI, how are u doin ' + name)
 names = names.split()
